85c48ad6268c40fd0effe9a5120fe9f781438a5e
AndriwRC <randriw4@gmail.com>
2023-08-07 18:57:00 -0500

    add about view & hyperlinks between views

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse(
+        "Rango says hey there partner! <br><a href='/rango/about/'>about</a>"
+    )
+
+
+def about(request):
+    return HttpResponse(
+        "Rango says here is the about page. <br><a href='/rango/'>index</a>"
+    )

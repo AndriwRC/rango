71a32a96cdcc5300871f0ba809ad0e688f1acdc3
AndriwRC <randriw4@gmail.com>
2023-08-30 17:30:03 -0500

    update index view: query Category model

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as
-    # its context. Note the key aboldmessage matches to
-    # {{ aboldmessage }} in the template!
+    # set up the context dictionary with our bold message
     context_dict = {"aboldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only '' or all if less than 5.
+    category_list = Category.objects.order_by("-likes")[:5]
+
+    # Place the list in our context_dict dictionary (with our aboldmessage!)
+    # that will be passed to the template engine.
+    context_dict["categories"] = category_list
+    # Render the response and send it back
+
     return render(request, "rango/index.html", context=context_dict)
 
 
